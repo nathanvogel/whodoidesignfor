eb114384b803d22ea17a080446a6159d00e18b25
Nathan Vogel <nathan@nathanvogel.com>
2019-02-07 10:20:08 +0100

    :arrow_up: Update to React 16 for .createRef()

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
+import ScrollMagic from 'scrollmagic';
 import styles from './styles.scss';
 import TitleSection from '../TitleSection';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    const controller = new ScrollMagic.Controller();
+    window.controller = controller;
+  }
+
   render() {
     return (
       <div className="main-root-content">
--- src/components/TitleSection/index.js
+++ src/components/TitleSection/index.js
@@ -1,46 +1,54 @@
-import React from 'react';
-import ScrollMagic from 'scrollmagic';
-import { TweenLite, TimelineMax, Power0, Expo } from 'gsap';
-import 'animation.gsap';
-import 'debug.addIndicators';
-import styles from './styles.scss';
+import React from "react";
+import ScrollMagic from "scrollmagic";
+import { TweenLite, TimelineMax, Power0, Expo } from "gsap";
+import "animation.gsap";
+import "debug.addIndicators";
+import styles from "./styles.scss";
 
 class TitleSection extends React.Component {
   constructor() {
     super();
-    this.loaderWrap = null;
-    this.loaderTween = null;
+    this.ref_main = React.createRef();
+    this.ref_wrapper = React.createRef();
+    this.ref_h1 = React.createRef();
   }
 
   componentDidMount() {
-    // Init ScrollMagic
-    const controller = new ScrollMagic.Controller();
     const parallaxTl = new TimelineMax();
-    parallaxTl.to(this.loaderWrap, 2, { opacity: 0, ease: Power0.easeNone }, 0);
+    parallaxTl.to(
+      this.ref_h1.current,
+      2,
+      { opacity: 0, ease: Power0.easeNone },
+      0
+    );
 
     const slideParallaxScene = new ScrollMagic.Scene({
-      triggerElement: this.loaderWrap,
+      triggerElement: this.ref_h1.current,
       triggerHook: 0.2,
-      duration: '20%',
+      duration: "20%",
     })
       .setTween(parallaxTl)
       .addIndicators({
-        name: 'fade scene',
-        colorTrigger: 'black',
+        name: "fade",
       })
-      .addTo(controller);
+      .addTo(window.controller);
+
+    const scene = new ScrollMagic.Scene({
+      triggerElement: this.ref_wrapper.current,
+      triggerHook: 0,
+      duration: "20%",
+    })
+      .setPin(this.ref_main.current)
+      .addIndicators({ name: "pin 1" }) // add indicators (requires plugin)
+      .addTo(window.controller);
   }
 
   render() {
     return (
-      <div className={styles.TitleSection}>
-        <h1
-          ref={div => {
-            this.loaderWrap = div;
-          }}
-        >
-          Who do I design for?
-        </h1>
+      <div ref={this.ref_wrapper}>
+        <div className={styles.TitleSection} ref={this.ref_main}>
+          <h1 ref={this.ref_h1}>Who do I design for?</h1>
+        </div>
       </div>
     );
   }
