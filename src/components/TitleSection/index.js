09e2c03d6b80eb6f87f49e045dbef465d9a756c4
Nathan Vogel <nathan@nathanvogel.com>
2019-02-07 01:49:43 +0100

    :sparkles: :heavy_plus_sign: Add scrollmagic and gsap

--- src/components/TitleSection/index.js
+++ src/components/TitleSection/index.js
@@ -1,11 +1,46 @@
 import React from 'react';
+import ScrollMagic from 'scrollmagic';
+import { TweenLite, TimelineMax, Power0, Expo } from 'gsap';
+import 'animation.gsap';
+import 'debug.addIndicators';
 import styles from './styles.scss';
 
 class TitleSection extends React.Component {
+  constructor() {
+    super();
+    this.loaderWrap = null;
+    this.loaderTween = null;
+  }
+
+  componentDidMount() {
+    // Init ScrollMagic
+    const controller = new ScrollMagic.Controller();
+    const parallaxTl = new TimelineMax();
+    parallaxTl.to(this.loaderWrap, 2, { opacity: 0, ease: Power0.easeNone }, 0);
+
+    const slideParallaxScene = new ScrollMagic.Scene({
+      triggerElement: this.loaderWrap,
+      triggerHook: 0.2,
+      duration: '20%',
+    })
+      .setTween(parallaxTl)
+      .addIndicators({
+        name: 'fade scene',
+        colorTrigger: 'black',
+      })
+      .addTo(controller);
+  }
+
   render() {
     return (
       <div className={styles.TitleSection}>
-        <h1>Who do I design for?</h1>
+        <h1
+          ref={div => {
+            this.loaderWrap = div;
+          }}
+        >
+          Who do I design for?
+        </h1>
       </div>
     );
   }
