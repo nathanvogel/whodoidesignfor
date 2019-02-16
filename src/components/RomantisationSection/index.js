f0921c3ef8817880b25f021111c5ad30b26fffac
Nathan Vogel <nathan@nathanvogel.com>
2019-02-16 18:42:53 +0100

    :lipstick: Tweak Romantisation section

--- src/components/RomantisationSection/index.js
+++ src/components/RomantisationSection/index.js
@@ -13,7 +13,7 @@ class RomantisationSection extends React.Component {
   constructor() {
     super();
     this.ref_pin_container = React.createRef();
-    this.ref_pin_content = React.createRef();
+    this.ref_section = React.createRef();
 
     this.ref_video_webcom = React.createRef();
     this.ref_video_strikethrough = React.createRef();
@@ -73,9 +73,20 @@ class RomantisationSection extends React.Component {
     tl.fromTo(
       this.ref_take_back.current,
       1,
-      { color: "#000", backgroundColor: "#fff" },
+      { color: "#000" },
       {
         color: "#fff",
+        repeat: 10,
+        ease: Linear.easeNone,
+        yoyo: false,
+      },
+      0
+    );
+    tl.fromTo(
+      this.ref_section.current,
+      1,
+      { backgroundColor: "#fff" },
+      {
         backgroundColor: "#000",
         repeat: 10,
         ease: Linear.easeNone,
@@ -85,13 +96,16 @@ class RomantisationSection extends React.Component {
     );
     tl.to(this.ref_take_back.current, 1, {
       color: "#000",
+      ease: Linear.easeNone,
+    });
+    tl.to(this.ref_section.current, 1, {
       backgroundColor: "#fff",
       ease: Linear.easeNone,
     });
     new ScrollMagic.Scene({
       triggerElement: this.ref_take_back.current,
-      triggerHook: 0,
-      duration: "30%",
+      triggerHook: 0.2,
+      duration: "50%",
     })
       .setTween(tl)
       // .addIndicators({ name: "take back" })
@@ -100,82 +114,87 @@ class RomantisationSection extends React.Component {
 
   render() {
     return (
-      <section ref={this.ref_pin_container}>
-        <div className={styles.RomantisationSection} ref={this.ref_pin_content}>
-          <div className={sStyles.Padder}>
-            <p>
-              I don’t want trendy brutalist side projects.
-              <br />I want brutal design on https://www.web.com
-            </p>
+      <section className={styles.RomantisationSection} ref={this.ref_section}>
+        <div className={sStyles.Padder}>
+          <p>
+            I don’t want trendy brutalist side projects.
+            <br />I want brutal design on https://www.web.com
+          </p>
 
-            <video
-              id="video"
-              className={styles.WebComVideo}
-              muted
-              autoPlay={false}
-              autostart={0}
-              ref={this.ref_video_webcom}
-            >
-              <source src="vids/web-com-intervention.mp4" type="video/mp4" />
-              Sorry, your browser does not support embedded videos.
-            </video>
-
-            <video
-              id="video"
-              className={styles.PaintStrikethroughVideo}
-              muted
-              autoPlay={false}
-              autostart={0}
-              ref={this.ref_video_strikethrough}
-            >
-              <source src="vids/paint-strikethrough.mp4" type="video/mp4" />
-              Sorry, your browser does not support embedded videos.
-            </video>
-
-            <video
-              id="video"
-              className={styles.PaintPeopleVideo}
-              muted
-              autoPlay={false}
-              autostart={0}
-              ref={this.ref_video_people}
-            >
-              <source src="vids/paint-people.mp4" type="video/mp4" />
-              Sorry, your browser does not support embedded videos.
-            </video>
-          </div>
-          <div className={sStyles.LeftBigColumnPadder}>
-            <p className={styles.Quote}>
-              “Getting wild with digital design in 2018 means getting wild in
-              2018 with responsive design that’s agnostic to the kind of device
-              you’re rocking. That’s doable, probably, but it’s really, really
-              hard.”
-            </p>
-            <p className={styles.QuoteSignature}>
-              - Tim Carmody,{" "}
-              <a href="https://kottke.org/18/01/foreclosing-on-the-future-of-the-book">
-                Jan 12, 2018 on kottke.org
-              </a>
-            </p>
-            <p className={styles.AndWith}>
-              And it means getting wild with accessibility.
-              <br /> And with performances.
-              <br /> And with engineering costs.
-              <br /> And with GDPR.
-              <br /> And with business goals.
-            </p>
-            <p className={`${sStyles.UtopianText} ${styles.AndIll}`}>
-              And I’ll fucking go wild.
-            </p>
-          </div>
-          <div
-            className={`${styles.TakeBack} ${sStyles.UtopianText}`}
-            ref={this.ref_take_back}
+          <video
+            id="video"
+            className={styles.WebComVideo}
+            muted
+            autoPlay={false}
+            autostart={0}
+            ref={this.ref_video_webcom}
           >
+            <source src="vids/web-com-intervention.mp4" type="video/mp4" />
+            Sorry, your browser does not support embedded videos.
+          </video>
+
+          <video
+            id="video"
+            className={styles.PaintStrikethroughVideo}
+            muted
+            autoPlay={false}
+            autostart={0}
+            ref={this.ref_video_strikethrough}
+          >
+            <source src="vids/paint-strikethrough.mp4" type="video/mp4" />
+            Sorry, your browser does not support embedded videos.
+          </video>
+
+          <video
+            id="video"
+            className={styles.PaintPeopleVideo}
+            muted
+            autoPlay={false}
+            autostart={0}
+            ref={this.ref_video_people}
+          >
+            <source src="vids/paint-people.mp4" type="video/mp4" />
+            Sorry, your browser does not support embedded videos.
+          </video>
+        </div>
+        <div className={sStyles.LeftBigColumnPadder}>
+          <p className={styles.Quote}>
+            “Getting wild with digital design in 2018 means getting wild in 2018
+            with responsive design that’s agnostic to the kind of device you’re
+            rocking. That’s doable, probably, but it’s really, really hard.”
+          </p>
+          <p className={styles.QuoteSignature}>
+            - Tim Carmody,{" "}
+            <a href="https://kottke.org/18/01/foreclosing-on-the-future-of-the-book">
+              Jan 12, 2018 on kottke.org
+            </a>
+          </p>
+          <p className={styles.AndWith}>
+            And it means getting wild with accessibility.
+            <br /> And with performances.
+            <br /> And with engineering costs.
+            <br /> And with GDPR.
+            <br /> And with business goals.
+          </p>
+          <p className={`${sStyles.UtopianText} ${styles.AndIll}`}>
+            And I’ll fucking go wild.
+          </p>
+        </div>
+        <div
+          className={`${styles.TakeBack} ${sStyles.UtopianText}`}
+          ref={this.ref_take_back}
+        >
+          <span>
             TAKE
             <br /> WEBDESIGN
             <br /> BACK
-          </div>
+          </span>
+        </div>
+        <div className={sStyles.LeftBigColumnPadder}>
+          <p className={`${sStyles.UtopianText}`}>
+            Take control of the awesome and unique properties of the digital
+            medium. It’s generative! It’s in motion! It’s unique for every user!
+          </p>
         </div>
       </section>
     );
